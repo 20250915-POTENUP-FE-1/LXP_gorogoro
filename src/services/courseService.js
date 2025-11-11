fe492ced3220d5dfb846f052e953011995842c83
syunghoon <shcho2109@gmail.com>
2025-11-11 09:41:52 +0900

    fix: getCategorieById 함수 반영

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,34 +1,3 @@
-import { collection, doc, getDoc, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase/config.js";
-//강의 및 카테고리 정보 CRUD 관련 서비스
-//강의 상세 조회 getCourseById()
-const COURSES_COLLECTION_NAME = "courses";
-/**
- * @param {string} courseId - 조회할 게시글 고유ID
- * @return {null|Object} 특정 게시글 객체(고유ID포함) 또는 null
- */
-export const getCourseById = async (courseId) => {
-  try {
-    const docSnapshot = await getDoc(
-      doc(db, COURSES_COLLECTION_NAME, courseId)
-    );
-    if (!docSnapshot.exists()) {
-      return null;
-    }
-    const docSnapshotData = docSnapshot.data();
-    return {
-      id: docSnapshot.id,
-      ...docSnapshotData,
-    };
-  } catch (error) {
-    throw error;
-  }
-};
-
-//강의 등록 createCourse()
-//강의 수정 updateCourse()
-//강의 삭제 deleteCourse()
-
 import {
   collection,
   doc,
@@ -96,4 +65,31 @@ const getFilteredCourses = async (filters) => {
   }
 };
 
-export { getCategories, getFilteredCourses };
+//강의 상세 조회 getCourseById()
+/**
+ * @param {string} courseId - 조회할 게시글 고유ID
+ * @return {null|Object} 특정 게시글 객체(고유ID포함) 또는 null
+ */
+const getCourseById = async (courseId) => {
+  try {
+    const docSnapshot = await getDoc(
+      doc(db, COURSES_COLLECTION_NAME, courseId)
+    );
+    if (!docSnapshot.exists()) {
+      return null;
+    }
+    const docSnapshotData = docSnapshot.data();
+    return {
+      id: docSnapshot.id,
+      ...docSnapshotData,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+//강의 등록 createCourse()
+//강의 수정 updateCourse()
+//강의 삭제 deleteCourse()
+
+export { getCategories, getFilteredCourses, getCourseById };
