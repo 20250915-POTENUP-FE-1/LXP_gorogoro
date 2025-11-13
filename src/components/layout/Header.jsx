ede66466043788a13496d693a0bd0d89f5d089be
syunghoon <shcho2109@gmail.com>
2025-11-13 12:23:01 +0900

    feat: userProfile 전역 상태에 따른 Header 동적 렌더링

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,8 +3,11 @@ import "./Header.css";
 
 import cartIconUrl from "/assets/icons/shopping-cart.svg";
 import userIconUrl from "/assets/icons/user.svg";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const userProfile = useSelector((state) => state.auth.userProfile);
+
   return (
     <header className="header">
       <div className="header__container">
@@ -12,55 +15,45 @@ function Header() {
           <NavLink to="/" className="header__brand">
             GORO
           </NavLink>
-          {/* <nav className="header__nav" aria-label="메인 메뉴">
-            <ul className="header__nav-list">
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  강좌
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  챌린지
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  멘토링
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  커뮤니티
-                </a>
-              </li>
-            </ul>
-          </nav> */}
+          {userProfile ? (
+            <div>{userProfile.displayName}님, 안녕하세요!</div>
+          ) : (
+            <div>로그인을 해보세요</div>
+          )}
         </div>
-        <div className="header__actions">
-          <NavLink to="/instructor" className="header__dashboard-link">
-            강사 대시보드
+        {userProfile ? (
+          <div className="header__actions">
+            {userProfile.role === "instructor" && (
+              <NavLink to="/instructor" className="header__dashboard-link">
+                강사 대시보드
+              </NavLink>
+            )}
+
+            <NavLink
+              to="/cart"
+              className="header__action-button"
+              aria-label="장바구니"
+            >
+              <img
+                src={cartIconUrl}
+                alt=""
+                className="header__action-icon"
+                aria-hidden="true"
+              />
+            </NavLink>
+            <NavLink
+              to="/mypage"
+              className="header__action-button"
+              aria-label="마이페이지"
+            >
+              <img src={userIconUrl} alt="" className="header__action-icon" />
+            </NavLink>
+          </div>
+        ) : (
+          <NavLink to="/login" className="header__dashboard-link">
+            로그인 하기
           </NavLink>
-          <NavLink
-            to="/cart"
-            className="header__action-button"
-            aria-label="장바구니"
-          >
-            <img
-              src={cartIconUrl}
-              alt=""
-              className="header__action-icon"
-              aria-hidden="true"
-            />
-          </NavLink>
-          <NavLink
-            to="/mypage"
-            className="header__action-button"
-            aria-label="마이페이지"
-          >
-            <img src={userIconUrl} alt="" className="header__action-icon" />
-          </NavLink>
-        </div>
+        )}
       </div>
     </header>
   );
