2beb11e166a5d8cd1ac9169986f3a00f9d09fd1b
christina-ahn <ysahn@tlksys.com>
2025-11-25 15:13:15 +0900

    fix: try-catch 오류 수정 및 const/dependecy 정리

--- src/pages/student/MyPage.jsx
+++ src/pages/student/MyPage.jsx
@@ -12,39 +12,44 @@ import { useSelector } from "react-redux";
 
 function MyPage() {
   const userProfile = useSelector((state) => state.auth.userProfile);
+  const USER_ID = userProfile?.id;
   const [enrolls, setEnrolls] = useState([]);
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getEnrollmentsById(USER_ID);
-      setEnrolls(result);
-      console.log(result);
+      try {
+        const result = await getEnrollmentsById(USER_ID);
+        setEnrolls(result);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    fetchData();
-  }, [userProfile?.id]);
+    if (USER_ID) {
+      fetchData();
+    }
+  }, [USER_ID]);
 
   useEffect(() => {
-    try {
-      const fetchData = async () => {
-        console.log(enrolls);
+    const fetchData = async () => {
+      try {
         // map()배열반환: 각 id에 대한 비동기 요청들을 배열로 만들고
         const tasks = enrolls.map((enroll) => getCourseById(enroll.courseId));
         // Promise.all로 병렬 처리 → 모든 상세가 완료되면 결과 배열을 받음
         const result = await Promise.all(tasks);
         setCourses(result ?? []);
-      };
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (enrolls.length > 0) {
       fetchData();
-    } catch (error) {
-      console.log(error);
     }
   }, [enrolls]);
 
   if (!userProfile) {
     return null;
   }
-
-  const USER_ID = userProfile.id;
 
   const handleCancel = async (courseId) => {
     if (window.confirm("정말로 수강을 취소하시겠습니까?")) {
