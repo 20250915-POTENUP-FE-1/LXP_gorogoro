ebfe40ca1e74cb1e6f10bbc0356ab05c0f9cf43b
syunghoon <shcho2109@gmail.com>
2025-12-10 06:24:45 +0900

    refac: formData, validate 함수 분리

--- src/features/intructor/action.ts
+++ src/features/intructor/action.ts
@@ -14,16 +14,10 @@ import { Chapter, Difficulty } from "../courses/types";
 type ActionState = {
   success: boolean;
   message?: string;
-  // errors는 폼 필드 유효성 검사 오류를 저장하는 데 사용됩니다.
-  // 예를 들어, { title: "제목은 필수입니다." } 와 같이 필드 이름과 오류 메시지를 매핑합니다.
-  // Record<string, string>은 키(필드 이름)와 값(오류 메시지)이 모두 문자열인 객체를 의미합니다.
   errors?: Record<string, string>;
 };
 
-export const CreateCourseAction = async (
-  prevState: ActionState,
-  formData: FormData
-): Promise<CourseFormResponse> => {
+const getCourseDataFromFormData = (formData: FormData): CourseFormRequest => {
   const title = formData.get("title") as string;
   const summary = formData.get("summary") as string;
   const categoryId = formData.get("categoryId") as string;
@@ -62,40 +56,7 @@ export const CreateCourseAction = async (
     chapterIndex++;
   }
 
-  // 유효성 검사 로직
-  const errors: Record<string, string> = {};
-
-  if (!title.trim()) {
-    errors.title = "강좌명을 입력하세요.";
-  }
-  if (!categoryId) {
-    errors.categoryId = "카테고리를 선택하세요.";
-  }
-  if (!difficulty) {
-    errors.difficulty = "난이도를 선택하세요.";
-  }
-  if (price <= 0) {
-    errors.price = "가격은 0원보다 크게 입력하세요.";
-  } else if (price >= 1000000) {
-    errors.price = "가격은 100만원 이상 설정할 수 없습니다.";
-  }
-  if (!summary.trim()) {
-    errors.summary = "강좌 요약을 입력하세요.";
-  }
-  if (!description.trim()) {
-    errors.description = "강좌 내용을 입력하세요.";
-  }
-  if (!summary.trim())
-    if (Object.keys(errors).length > 0) {
-      // 모든 유효성 검사 후 오류가 하나라도 있으면 한번에 반환
-      return {
-        success: false,
-        message: "입력 값을 확인해주세요.",
-        errors,
-      };
-    }
-
-  const newCourse: CourseFormRequest = {
+  return {
     title,
     summary,
     categoryId,
@@ -106,6 +67,50 @@ export const CreateCourseAction = async (
     contents,
     availableDays,
   };
+};
+
+const validateCourseData = (data: CourseFormRequest) => {
+  const errors: Record<string, string> = {};
+
+  if (!data.title.trim()) {
+    errors.title = "강좌명을 입력하세요.";
+  }
+  if (!data.categoryId) {
+    errors.categoryId = "카테고리를 선택하세요.";
+  }
+  if (!data.difficulty) {
+    errors.difficulty = "난이도를 선택하세요.";
+  }
+  if (data.price <= 0) {
+    errors.price = "가격은 0원보다 크게 입력하세요.";
+  } else if (data.price >= 1000000) {
+    errors.price = "가격은 100만원 이상 설정할 수 없습니다.";
+  }
+  if (!data.summary.trim()) {
+    errors.summary = "강좌 요약을 입력하세요.";
+  }
+  if (!data.description.trim()) {
+    errors.description = "강좌 내용을 입력하세요.";
+  }
+  return errors;
+};
+
+export const CreateCourseAction = async (
+  prevState: ActionState,
+  formData: FormData
+): Promise<CourseFormResponse> => {
+  const newCourse = getCourseDataFromFormData(formData);
+
+  // 유효성 검사 로직
+  const errors = validateCourseData(newCourse);
+  if (Object.keys(errors).length > 0) {
+    // 모든 유효성 검사 후 오류가 하나라도 있으면 한번에 반환
+    return {
+      success: false,
+      message: "입력 값을 확인해주세요.",
+      errors,
+    };
+  }
 
   try {
     await createCourse(newCourse);
@@ -130,90 +135,22 @@ export const UpdateCourseAction = async (
   formData: FormData
 ): Promise<CourseFormResponse> => {
   const title = formData.get("title") as string;
-  const summary = formData.get("summary") as string;
   const categoryId = formData.get("categoryId") as string;
-  const price = formData.get("price") as unknown as number;
-  const coverImageUrl = formData.get("coverImageUrl") as string;
-  const description = formData.get("description") as string;
-  const difficulty = formData.get("difficulty") as Difficulty;
-  const availableDays = formData.get("availableDays") as unknown as number;
-
-  // FormData에서 contents 배열 재구성
-  const contents: Chapter[] = [];
-  let chapterIndex = 0;
-  while (formData.has(`contents[${chapterIndex}][chapterTitle]`)) {
-    const chapterTitle = formData.get(
-      `contents[${chapterIndex}][chapterTitle]`
-    ) as string;
-
-    const lessons: { title: string; seq: number; resourceUrl: string }[] = [];
-    let lessonIndex = 0;
-    while (
-      formData.has(`contents[${chapterIndex}][lessons][${lessonIndex}][title]`)
-    ) {
-      lessons.push({
-        title: formData.get(
-          `contents[${chapterIndex}][lessons][${lessonIndex}][title]`
-        ) as string,
-        seq: lessonIndex + 1,
-        resourceUrl: formData.get(
-          `contents[${chapterIndex}][lessons][${lessonIndex}][resourceUrl]`
-        ) as string,
-      });
-      lessonIndex++;
-    }
-
-    contents.push({ chapterTitle, seq: chapterIndex + 1, lessons });
-    chapterIndex++;
-  }
+  const updatedCourse = getCourseDataFromFormData(formData);
 
   // 유효성 검사 로직
-  const errors: Record<string, string> = {};
-
-  if (!title.trim()) {
-    errors.title = "강좌명을 입력하세요.";
+  const errors = validateCourseData(updatedCourse);
+  if (Object.keys(errors).length > 0) {
+    // 모든 유효성 검사 후 오류가 하나라도 있으면 한번에 반환
+    return {
+      success: false,
+      message: "입력 값을 확인해주세요.",
+      errors,
+    };
   }
-  if (!categoryId) {
-    errors.categoryId = "카테고리를 선택하세요.";
-  }
-  if (!difficulty) {
-    errors.difficulty = "난이도를 선택하세요.";
-  }
-  if (price <= 0) {
-    errors.price = "가격은 0원보다 크게 입력하세요.";
-  } else if (price >= 1000000) {
-    errors.price = "가격은 100만원 이상 설정할 수 없습니다.";
-  }
-  if (!summary.trim()) {
-    errors.summary = "강좌 요약을 입력하세요.";
-  }
-  if (!description.trim()) {
-    errors.description = "강좌 내용을 입력하세요.";
-  }
-  if (!summary.trim())
-    if (Object.keys(errors).length > 0) {
-      // 모든 유효성 검사 후 오류가 하나라도 있으면 한번에 반환
-      return {
-        success: false,
-        message: "입력 값을 확인해주세요.",
-        errors,
-      };
-    }
-
-  const newCourse: CourseFormRequest = {
-    title,
-    summary,
-    categoryId,
-    price,
-    coverImageUrl,
-    description,
-    difficulty,
-    contents,
-    availableDays,
-  };
 
   try {
-    await updateCourse(id, newCourse);
+    await updateCourse(id, updatedCourse);
   } catch (error) {
     return {
       success: false,
@@ -229,7 +166,7 @@ export const UpdateCourseAction = async (
   redirect("/instructor/courses");
 };
 
-export const deleteCourseAction = async (id: string) => {
+export const DeleteCourseAction = async (id: string) => {
   await deleteCourse(id);
   revalidatePath("/instructor/courses");
 };
