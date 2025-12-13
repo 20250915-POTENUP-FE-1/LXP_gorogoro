21e365a8a5a599a519d380f1f13e9f014da620bd
syunghoon <shcho2109@gmail.com>
2025-12-13 16:06:38 +0900

    fix: cart 서비스 함수 수정

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,30 +1,40 @@
-import { get, post, del } from "@/shared/lib/api";
+import { fetchWithAuth } from "@/shared/lib/serverApi";
 
 const CARTS_ENDPOINT = "carts";
 
 export const getCart = async () => {
-  const response = await get(CARTS_ENDPOINT);
+  const response = await fetchWithAuth(CARTS_ENDPOINT, {
+    method: "GET",
+  });
   if (response.error) throw response.error;
   return response.data;
 };
 
 //장바구니 등록
 export const addToCart = async (courseId: string) => {
-  const response = await post(CARTS_ENDPOINT, { courseId });
+  const response = await fetchWithAuth(CARTS_ENDPOINT, {
+    method: "POST",
+    body: JSON.stringify({ courseId }),
+  });
   if (response.error) throw response.error;
   return response.data;
 };
 
 //장바구니 전체 삭제
 export const deleteAllCart = async () => {
-  const response = await del(CARTS_ENDPOINT);
+  const response = await fetchWithAuth(CARTS_ENDPOINT, {
+    method: "DELETE",
+  });
   if (response.error) throw response.error;
   return response.data;
 };
 
 //장바구니 선택 삭제
 export const deleteCartItem = async (courseId: number) => {
-  const response = await del(`${CARTS_ENDPOINT}/items`, { courseId });
+  const response = await fetchWithAuth(`${CARTS_ENDPOINT}/items`, {
+    method: "DELETE",
+    body: JSON.stringify({ courseId }),
+  });
   if (response.error) throw response.error;
   return response.data;
 };
