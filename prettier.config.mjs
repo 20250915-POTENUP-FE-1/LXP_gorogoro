086eaa892dc477da520c3668303d238a969f3c1d
Chang-KY <pzxcvp0508@gmail.com>
2026-01-03 16:42:36 +0900

    chore: prettier config파일 셋팅

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+}
+
+export default config
