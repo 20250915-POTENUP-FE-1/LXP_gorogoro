c614ebe60a647ce157b0855cf9a83b1f7193c68f
Chang-KY <pzxcvp0508@gmail.com>
2026-01-13 09:33:23 +0900

    refactor: 강사 asidebar 제거

--- src/app/(auth)/mypage/layout.tsx
+++ src/app/(auth)/mypage/layout.tsx
@@ -1,14 +1,10 @@
-import MySidebar from "@/features/mypage/components/MySidebar";
-import styles from "./layout.module.css";
+import MySidebar from '@/features/mypage/components/MySidebar';
+import styles from './layout.module.css';
 
-export default async function MyPageLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function MyPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className={styles.page}>
-      <div className={`page-wrapper ${styles.container}`}>
+      <div className={`${styles.container}`} style={{ paddingRight: '3rem', paddingLeft: '3rem' }}>
         <MySidebar />
         {children}
       </div>
